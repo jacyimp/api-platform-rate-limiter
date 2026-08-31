872b7579f72766f6aba78106ccf045c0ed44075f
Jacyimp <supercasterjacy@gmail.com>
2026-08-31 16:07:27 +0330

    chore: configure development tooling

---
+++ behat.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use Behat\Config\Config;
+use Behat\Config\Profile;
+use Behat\Config\Suite;
+use Jacyimp\ApiPlatformRateLimiter\Tests\Behaviour\FeatureContext;
+
+return (new Config())
+    ->withProfile(
+        profile: (new Profile('default'))
+            ->withSuite(
+                suite: (new Suite('package'))
+                    ->withPaths('%paths.base%/features')
+                    ->withContexts(FeatureContext::class),
+            ),
+    );
---
+++ tests/Behaviour/FeatureContext.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jacyimp\ApiPlatformRateLimiter\Tests\Behaviour;
+
+use Behat\Behat\Context\Context;
+
+final class FeatureContext implements Context
+{
+}
